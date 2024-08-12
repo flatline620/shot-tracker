491fe424fea2f2c11f918f8869d70b2184a06fa0
Rob Kiser <rob.kiser@bitdirect.com>
2024-08-12 08:56:30 -0400

    Changed default shooter

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ import './App.css';
 
 const App = () => {
   const [games, setGames] = useState([
-    { name: "Steak and Clays", date: "2024-08-01", location: "TBSC", numStations: 9, minShots: 4, maxShots: 8, totalShots: 50, shooters: [] },
-    { name: "Game 2", date: "2024-08-02", location: "Location 2", numStations: null, minShots: null, maxShots: null, totalShots: null, shooters: [] },
+    { name: "Steak and Clays", date: "2024-08-01", location: "TBSC", numStations: 7, minShots: 4, maxShots: 8, totalShots: 50, shooters: [{name: 'Sam', shotsTaken: 0, numHits: 0, maxScore: 0}, {name: 'Laura', shotsTaken: 0, numHits: 0, maxScore: 0}, {name: 'Rob', shotsTaken: 0, numHits: 0, maxScore: 0}] },
   ]);
 
   const handleAddGame = (newGame) => {
